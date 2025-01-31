a3056619741173eca087c7248f9319fb978ac5f8
Binay T <abhi10sheksharma@gmail.com>
2025-01-31 19:26:19 +0530

    CI attempt 8

--- documentqa_backend/documentqa_backend/settings.py
+++ documentqa_backend/documentqa_backend/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
         'NAME': os.environ.get("DATABASE_NAME"),
         'USER': os.environ.get("DATABASE_USER"),
         'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
-        'HOST': os.environ.get("DATABASE_HOST", "postgres"),
-        'PORT': os.environ.get("DATABASE_PORT", "5432"),
+        'HOST': os.environ.get("DATABASE_HOST"),
+        'PORT': os.environ.get("DATABASE_PORT"),
     }
 }
 
