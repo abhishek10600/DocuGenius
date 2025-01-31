c0d806243761c5e26a555db0d25d057f2222dabe
Binay T <abhi10sheksharma@gmail.com>
2025-01-31 15:12:46 +0530

    Updated settings.py again

--- documentqa_backend/documentqa_backend/settings.py
+++ documentqa_backend/documentqa_backend/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [os.getenv("FRONTEND_DEV_URL")]
+CORS_ALLOWED_ORIGINS = [os.getenv("FRONTEND_DEV_URL", "http://localhost:5173")]
 
 CORS_ALLOW_CREDENTIALS = True
 
