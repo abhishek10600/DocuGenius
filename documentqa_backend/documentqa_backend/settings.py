e3031171446dfb645a231cd53d23cb4edcfb957a
Binay T <abhi10sheksharma@gmail.com>
2025-01-31 14:49:46 +0530

    CI workflow updated and settings.py updated

--- documentqa_backend/documentqa_backend/settings.py
+++ documentqa_backend/documentqa_backend/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "django-insecure-0q)f!e656bs64y9pv3$t$e&ox^0vay-30i=6^8!v_ia=3dl3v%")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
